da7ffb4d5df9ff05c53c26ecd7162b9ed7ee9511
wobushicsy <csy657703828@163.com>
2023-04-29 21:19:43 +0800

    Task2 done

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,12 @@
+import sun.awt.image.ImageWatched;
+
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        LinkedListDeque<Character> charList = new LinkedListDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            charList.addLast(word.charAt(i));
+        }
+
+        return charList;
+    }
+}
