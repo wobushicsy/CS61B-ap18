ceb78b30e67f7931fe3bb11a676ee31f7fc7c42b
wobushicsy <csy657703828@163.com>
2023-04-29 22:23:57 +0800

    Task5 done

--- proj1b/TestOffByN.java
+++ proj1b/TestOffByN.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 public class TestOffByN {
     static OffByN offBy5 = new OffByN(5);
 
+    @Test
     public void testOffByN() {
         assertTrue(offBy5.equalChars('a', 'f'));  // true
         assertFalse(offBy5.equalChars('\n', 'S'));  // false
